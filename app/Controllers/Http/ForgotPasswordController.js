31952d1dc0db6992e70d3fb7b75e8048967f69d2
Future Solucoes <futuresolucoes.tecnologia@gmail.com>
2019-10-09 15:10:22 -0300

    Add route forgot_password to recovery it

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,11 +2,14 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
     try {
       const email = request.input('email')
+
+      console.log(request.input)
 
       const user = await User.findByOrFail('email', email)
 
@@ -14,8 +17,19 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['mails.forgot_password', 'mails.forgot_password-text'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message
+            .to(user.email)
+            .from('noreply@futuresolucoes.com.br', 'Equipe Future Soluções')
+            .subject('Recovery password')
+        }
+      )
     } catch (error) {
-      return response.status(error.status).send({ error: { message: 'E-mail does not exist' } })
+      return response.status(error.status).send({ error: { message: error.message } })
     }
   }
 }
