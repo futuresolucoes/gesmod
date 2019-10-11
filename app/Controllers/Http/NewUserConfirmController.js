96f0034eac25cb34a60fdb7c07b55334a0c70aad
welitonhs <weliton.sernajotto23@gmail.com>
2019-10-11 10:49:41 -0300

    Added route to confirm new user and added route to send mail for new user. Created ConfirmNewUserService to sendmail in more of one controllers

---
+++ app/Controllers/Http/NewUserConfirmController.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const crypto = require('crypto')
+const subDays = require('date-fns/subDays')
+const isAfter = require('date-fns/isAfter')
+
+const ConfirmNewUser = use('App/Services/ConfirmNewUserService')
+const User = use('App/Models/User')
+
+class NewUserConfirmController {
+  async store ({ request, response }) {
+    try {
+      const { email } = request.all()
+
+      const user = await User.findByOrFail('email', email)
+
+      if (user.is_active) {
+        return response.status(400).send({ error: { message: 'User is already active' } })
+      }
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await ConfirmNewUser.run({ name: user.name, email: user.email, token: user.token, link: 'https://teste' })
+    } catch (error) {
+      return response.status(400).send({ error: { message: 'Email not found' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      if (user.is_active) {
+        return response.status(400).send({ error: { message: 'User is already active' } })
+      }
+
+      const tokenExpired = isAfter(subDays(new Date(), 1), user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({ error: { message: 'Token expired' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.is_active = 1
+
+      user.save()
+    } catch (error) {
+      return response.status(error.status).send({ error: { message: 'Token invalid' } })
+    }
+  }
+}
+
+module.exports = NewUserConfirmController
--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,14 +1,32 @@
 'use strict'
 
+const crypto = require('crypto')
+
+const ConfirmNewUser = use('App/Services/ConfirmNewUserService')
 const User = use('App/Models/User')
 
 class UserController {
-  async store ({ request }) {
-    const data = request.only(['name', 'fantasy_name', 'email', 'password'])
+  async store ({ request, response }) {
+    try {
+      const data = request.only(['name', 'fantasy_name', 'email', 'password'])
 
-    const user = await User.create(data)
+      const user = await User.findBy('email', data.email)
 
-    return user
+      if (user) {
+        return response.status(400).send({ error: { message: 'E-mail already registered' } })
+      }
+
+      data.token = crypto.randomBytes(10).toString('hex')
+      data.token_created_at = new Date()
+
+      const newUser = await User.create(data)
+
+      await ConfirmNewUser.run({ name: data.name, email: data.email, token: data.token, link: 'https://teste' })
+
+      return newUser
+    } catch (error) {
+      return response.status(400).send({ error: { message: 'Confirm your data' } })
+    }
   }
 }
 
